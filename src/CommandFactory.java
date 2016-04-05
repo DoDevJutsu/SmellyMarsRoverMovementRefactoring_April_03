64b74162dac92bc87864649ad21a84c30b531e74
trikitrok <trikitrok@gmail.com>
2016-04-05 16:35:18 +0200

    Completely removed switch using a Map

--- src/CommandFactory.java
+++ src/CommandFactory.java
@@ -1,37 +1,27 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CommandFactory {
+    private static final String FORWARDS_DISPLACEMENT = "f";
+    private static final String BACKWARDS_DISPLACEMENT = "b";
+    private static final String LEFT_ROTATION = "l";
+    private static final String RIGHT_ROTATION = "r";
+    private static Map<String, Command> knownCommands = knownCommands();
+
     public static Command create(String representation) {
-        if (isLeftRotation(representation)) {
-            return new LeftRotation();
-        } else if (isRightRotation(representation)) {
-            return new RightRotation();
-        } else if (isForwardDisplacement(representation) || isBackwardsDisplacement(representation)){
-            return new Displacement(getLength(representation));
+        if (!knownCommands.containsKey(representation)) {
+            return new UnknownCommand();
         }
-
-        return new UnknownCommand();
+        return knownCommands.get(representation);
     }
 
-    private static int getLength(String representation) {
-        final int LENGTH = 1;
-        if (isForwardDisplacement(representation)) {
-            return LENGTH;
-        }
-        return -LENGTH;
-    }
-
-    private static boolean isLeftRotation(String representation) {
-        return representation.equals("l");
-    }
-
-    private static boolean isRightRotation(String representation) {
-        return representation.equals("r");
-    }
-
-    private static boolean isForwardDisplacement(String representation) {
-        return representation.equals("f");
-    }
-
-    private static boolean isBackwardsDisplacement(String representation) {
-        return representation.equals("b");
+    private static Map<String, Command> knownCommands() {
+        Map<String, Command> knownCommands = new HashMap<>();
+        knownCommands.put(LEFT_ROTATION, new LeftRotation());
+        knownCommands.put(RIGHT_ROTATION, new RightRotation());
+        knownCommands.put(FORWARDS_DISPLACEMENT, new Displacement(1));
+        knownCommands.put(BACKWARDS_DISPLACEMENT, new Displacement(-1));
+        return Collections.unmodifiableMap(knownCommands);
     }
 }
